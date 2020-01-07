36f184ad90b97101f9d3d78856ca5ab945320440
Kevin Castillo <kevincastillo318@gmail.com>
2020-01-07 21:01:01 +0000

    Domain name generator

--- src/js/index.js
+++ src/js/index.js
@@ -6,6 +6,18 @@ import "../assets/img/4geeks.ico";
 //import 'breathecode-dom'; //DOM override to make JS easier to use
 import "../style/index.scss";
 
-window.onload = function() {
-  console.log("Hello Rigo from the console!");
-};
+var pronoun = ["The", "Our", "Your", "That"];
+var adj = ["Great", "Big", "Small", "Tiny"];
+var noun = ["Mandalorian", "Batman", "Kevin", "Yeezy"];
+var ending = [".com", ".net", ".org", ".edu"];
+let temp = "";
+for (let a of pronoun) {
+  for (let b of adj) {
+    for (let c of noun) {
+      for (let d of ending) {
+        temp += "<li>" + a + b + c + d + "</li>";
+      }
+    }
+  }
+}
+document.querySelector("ul").innerHTML = temp;
